8c8025309c7f5962e580403dba7b7ff1ed457c51
pantaLuc <lucapameni@gmail.com>
2023-07-12 20:46:58 +0200

    feat(MonAuto) : Working on Puissance

--- src/main/java/fr/dojo/monauto/web/graphql/input/CategorieInput.java
+++ src/main/java/fr/dojo/monauto/web/graphql/input/CategorieInput.java
@@ -1,4 +1,17 @@
 package fr.dojo.monauto.web.graphql.input;
 
+import fr.dojo.monauto.data.entity.Categorie;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategorieInput {
+    private String nomCategorie;
+
+    public Categorie getEntity(){
+        Categorie categorie=new Categorie() ;
+        categorie.setNomCategorie(this.nomCategorie);
+        return categorie ;
+    }
 }
--- src/main/java/fr/dojo/monauto/web/graphql/ressource/CategorieRessource.java
+++ src/main/java/fr/dojo/monauto/web/graphql/ressource/CategorieRessource.java
@@ -1,4 +1,27 @@
 package fr.dojo.monauto.web.graphql.ressource;
 
+
+import fr.dojo.monauto.data.entity.Categorie;
+import fr.dojo.monauto.service.CategorieService;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+
+import java.util.List;
+
+@GraphQLApi
 public class CategorieRessource {
+    private  final CategorieService categorieService ;
+
+    public CategorieRessource(CategorieService categorieService) {
+        this.categorieService = categorieService;
+    }
+
+    @Query
+    @Description("List of all vehicules'categories")
+    public List<Categorie> allCategorie(){
+        return this.categorieService.categorieList();
+    }
+
+    //TODO : ADD other methods
 }
