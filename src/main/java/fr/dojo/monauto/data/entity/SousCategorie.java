8968be192a03233ae5372bbad4dad1aa73bf95cb
pantaLuc <lucapameni@gmail.com>
2023-07-12 20:55:43 +0200

    feat(Mon auto) : adding sous Categorie

--- src/main/java/fr/dojo/monauto/data/entity/SousCategorie.java
+++ src/main/java/fr/dojo/monauto/data/entity/SousCategorie.java
@@ -1,6 +1,19 @@
 package fr.dojo.monauto.data.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="sousCategorie")
+@Getter
+@Setter
 public class SousCategorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idSousCategorie")
     private int idSousCategorie;
+    @Column(name="libelleSousCategorie")
     private String libelleSousCategorie;
 }
